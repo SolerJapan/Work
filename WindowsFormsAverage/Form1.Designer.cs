d4813dd927d18b668c5404805dcab782bd675c5e
Alex S <55304780+SolerJapan@users.noreply.github.com>
2022-09-28 11:26:25 -0400

    Add files via upload

---
+++ WindowsFormsAverage/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsAverage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numBox1 = new System.Windows.Forms.TextBox();
+            this.numBox2 = new System.Windows.Forms.TextBox();
+            this.numBox3 = new System.Windows.Forms.TextBox();
+            this.numBox4 = new System.Windows.Forms.TextBox();
+            this.numBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numBox1
+            // 
+            this.numBox1.Location = new System.Drawing.Point(120, 87);
+            this.numBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numBox1.Name = "numBox1";
+            this.numBox1.Size = new System.Drawing.Size(132, 22);
+            this.numBox1.TabIndex = 0;
+            // 
+            // numBox2
+            // 
+            this.numBox2.Location = new System.Drawing.Point(120, 140);
+            this.numBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numBox2.Name = "numBox2";
+            this.numBox2.Size = new System.Drawing.Size(132, 22);
+            this.numBox2.TabIndex = 1;
+            // 
+            // numBox3
+            // 
+            this.numBox3.Location = new System.Drawing.Point(120, 191);
+            this.numBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numBox3.Name = "numBox3";
+            this.numBox3.Size = new System.Drawing.Size(132, 22);
+            this.numBox3.TabIndex = 2;
+            // 
+            // numBox4
+            // 
+            this.numBox4.Location = new System.Drawing.Point(120, 238);
+            this.numBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numBox4.Name = "numBox4";
+            this.numBox4.Size = new System.Drawing.Size(132, 22);
+            this.numBox4.TabIndex = 3;
+            // 
+            // numBox5
+            // 
+            this.numBox5.Location = new System.Drawing.Point(120, 289);
+            this.numBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numBox5.Name = "numBox5";
+            this.numBox5.Size = new System.Drawing.Size(132, 22);
+            this.numBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 148);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 198);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Number3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 238);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Number4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 289);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Number5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 52);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Give Me 5 numbers";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(655, 393);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Average";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(882, 453);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numBox5);
+            this.Controls.Add(this.numBox4);
+            this.Controls.Add(this.numBox3);
+            this.Controls.Add(this.numBox2);
+            this.Controls.Add(this.numBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numBox1;
+        private System.Windows.Forms.TextBox numBox2;
+        private System.Windows.Forms.TextBox numBox3;
+        private System.Windows.Forms.TextBox numBox4;
+        private System.Windows.Forms.TextBox numBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
